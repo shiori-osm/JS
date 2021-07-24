6518a8b8cbabcfaa3f1533950a866d99ead87473
しおり.大隅 <metalgear.shiorid@gmail.com>
2021-07-24 15:06:31 +0900

    しつもん

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 "use script";
 {
-  const open = document.getElementById("open");
-  const close = document.getElementById("close");
-  const closeMenu = document.querySelector(".close-hg-menu");
+  // 自分の↓
+  // const open = document.getElementById("open");
+  // const close = document.getElementById("close");
+  // const closeMenu = document.querySelector(".close-hg-menu");
   
+  // open.addEventListener("click" , ()=>{
+  //   closeMenu.classList.add("show");
+  // });
+  // close.addEventListener("click" , ()=>{
+  //   closeMenu.classList.remove("show");
+  // });
 
 
-  open.addEventListener("click" , ()=>{
-    closeMenu.classList.add("show");
-  });
-  close.addEventListener("click" , ()=>{
-    closeMenu.classList.remove("show");
-  });
+
+// 教科書の↓
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
+});
+
 }
 
 
